b19b8523c6172035bc56f69d92200dcfb855b3a6
nicolasqvl <nicolas.qvl@gmail.com>
2023-04-03 15:15:43 +0200

    feat(security): registration and login controller and form

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Unit;
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'label' => 'Identifiant de l\'administrateur',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a username',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your username should be at least {{ limit }} characters',
+                    ])
+                ],
+            ])
+
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+            ])
+
+            ->add('unit', EntityType::class, [
+                'label' => 'Unité de rattachement',
+                'class' => Unit::class,
+                'choice_label' => 'name',
+            ])
+
+            ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'label' => 'Mot de passe',
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+
+            ->add('roles', ChoiceType::class, [
+                'label' => 'Rôle du compte',
+                'choices' => ['ROLE_ADMIN' => 'ROLE_ADMIN', 'ROLE_SUPER_ADMIN' => 'ROLE_SUPER_ADMIN'],
+                'expanded' => true,
+                'multiple' => false,
+            ]);
+
+            // Symfony's documentation : how change array's value to stirng
+            $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                    // transform the array to a string
+                    return count($rolesArray)? $rolesArray[0]: null;
+                },
+                function ($rolesString) {
+                    // transform the string back to an array
+                    return [$rolesString];
+                }
+            ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
